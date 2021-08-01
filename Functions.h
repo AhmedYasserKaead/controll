50b804e20c88dd4c7ba1353487c6b6929f78a0b2
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-08-01 15:48:51 +0200

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,15 @@
+//
+// Created by ayaser on 8/1/2021.
+//
+#include <stdint.h>
+
+#ifndef CONTROL_FUNCTIONS_H
+#define CONTROL_FUNCTIONS_H
+
+uint8_t Is_button_pressed();
+void Set_LED(uint8_t x);
+void Control_LED();
+
+
+
+#endif //CONTROL_FUNCTIONS_H
